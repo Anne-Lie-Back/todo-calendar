4c881275c45522043df948a1e49c45238bb99cf7
alexbryw <alexbryw@gmail.com>
2019-11-27 19:26:43 +0100

    Changed calendar-container.css file name and added addDaysToCalendar fuction.

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -1,3 +1,14 @@
 function calendarStart(){
     console.log('Calendar Start2.')
+    addDaysToCalendar();
+}
+
+function addDaysToCalendar(){
+
+    for(let i = 0; i < 31; i++){
+        let makeDiv = document.createElement("div");
+        let makeText = document.createTextNode("abc  " + i);
+        makeDiv.appendChild(makeText);
+        document.querySelector('.calendar-container').append(makeDiv);
+    }
 }
