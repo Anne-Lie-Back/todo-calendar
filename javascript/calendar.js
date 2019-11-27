28756b2e39a0fd6270366fd60ba862355c6df594
alexbryw <alexbryw@gmail.com>
2019-11-27 20:01:33 +0100

    Added class name to new calendar day divs and id tag for date.

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -5,10 +5,22 @@ function calendarStart(){
 
 function addDaysToCalendar(){
 
+    fillEmptyDays(3);
+
     for(let i = 0; i < 31; i++){
         let makeDiv = document.createElement("div");
+        makeDiv.className = "calendar-day";
+        makeDiv.id = "2019-11-" + i;
         let makeText = document.createTextNode("abc  " + i);
         makeDiv.appendChild(makeText);
         document.querySelector('.calendar-container').append(makeDiv);
+    }
+}
+
+function fillEmptyDays(nrOfEmptyDays){
+    for(let i = 0; i < nrOfEmptyDays; i++){
+        let makeDiv = document.createElement('div');
+        makeDiv.className = "empty-day";
+        document.querySelector('.calendar-container').appendChild(makeDiv);
     }
 }
