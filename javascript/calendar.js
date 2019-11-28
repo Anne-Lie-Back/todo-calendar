51258ee320e02aab1d58b922fb2c112cfc4f9b90
alexbryw <alexbryw@gmail.com>
2019-11-28 11:16:25 +0100

    Added getDaysInMonth and removed old days for month change.

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -7,7 +7,7 @@ function addDaysToCalendar(){
 
     fillEmptyDays(3);
 
-    for(let i = 0; i < 31; i++){
+    for(let i = 0; i < getDaysInMonth(); i++){
         let makeDiv = document.createElement("div");
         makeDiv.className = "calendar-day";
         makeDiv.id = "2019-11-" + i;
@@ -15,6 +15,9 @@ function addDaysToCalendar(){
         makeDiv.appendChild(makeText);
         document.querySelector('.calendar-container').append(makeDiv);
     }
+    console.log("done days ");
+    //removeAllDays();
+    //getDaysInMonth();
 }
 
 function fillEmptyDays(nrOfEmptyDays){
@@ -23,4 +26,24 @@ function fillEmptyDays(nrOfEmptyDays){
         makeDiv.className = "empty-day";
         document.querySelector('.calendar-container').appendChild(makeDiv);
     }
+}
+
+function removeAllDays(){
+    const allDaysList = document.querySelectorAll('.calendar-day');
+    const emptyDaysList = document.querySelectorAll('.empty-day')
+    console.log(allDaysList , emptyDaysList);
+    for(let days of allDaysList){
+        days.remove();
+    }
+
+    for(let emptyDay of emptyDaysList){
+        emptyDay.remove();
+    }
+    console.log(allDaysList , emptyDaysList);
+}
+
+function getDaysInMonth(){
+    let nrDaysInMonth = new Date(2019, 11, 0).getDate();
+    console.log('days ' + nrDaysInMonth);
+    return nrDaysInMonth;
 }
