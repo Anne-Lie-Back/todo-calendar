cc720d9d22c6983db4445f3fa1a41e70fd0c4db4
alexbryw <alexbryw@gmail.com>
2019-11-28 21:08:07 +0100

    Added get first day of month and next month fuctions.

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -1,17 +1,24 @@
+let currentViewYear;
+let currentViewMonth;
+
 function calendarStart(){
     console.log('Calendar Start2.')
-    addDaysToCalendar();
+    currentViewYear = new Date().getFullYear();
+    currentViewMonth = new Date().getMonth();
+    console.log(currentViewYear,currentViewMonth);
+    addDaysToCalendar(currentViewYear,currentViewMonth);
+    //addDaysToCalendar(2020,1);
 }
 
-function addDaysToCalendar(){
-
-    fillEmptyDays(3);
-
-    for(let i = 0; i < getDaysInMonth(); i++){
+function addDaysToCalendar(setYear,setMonth){
+    removeAllDays();//clear old month view.
+    console.log(setYear,setMonth);
+    fillEmptyDays(getFirstWeekdayOfMonth(setYear,setMonth));
+    for(let i = 0; i < getDaysInMonth(setYear,setMonth); i++){
         let makeDiv = document.createElement("div");
         makeDiv.className = "calendar-day";
-        makeDiv.id = "2019-11-" + i;
-        let makeText = document.createTextNode("abc  " + i);
+        makeDiv.id = "2019-11-" + (i +1 );
+        let makeText = document.createTextNode("abc  " + (i +1 ));
         makeDiv.appendChild(makeText);
         document.querySelector('.calendar-container').append(makeDiv);
     }
@@ -21,7 +28,11 @@ function addDaysToCalendar(){
 }
 
 function fillEmptyDays(nrOfEmptyDays){
-    for(let i = 0; i < nrOfEmptyDays; i++){
+    if(nrOfEmptyDays === 0){ //sunday is zero so set to 7.
+        nrOfEmptyDays = 7;
+    }
+    console.log('nr days '+nrOfEmptyDays);
+    for(let i = 0; i < (nrOfEmptyDays-1); i++){
         let makeDiv = document.createElement('div');
         makeDiv.className = "empty-day";
         document.querySelector('.calendar-container').appendChild(makeDiv);
@@ -31,19 +42,51 @@ function fillEmptyDays(nrOfEmptyDays){
 function removeAllDays(){
     const allDaysList = document.querySelectorAll('.calendar-day');
     const emptyDaysList = document.querySelectorAll('.empty-day')
-    console.log(allDaysList , emptyDaysList);
-    for(let days of allDaysList){
-        days.remove();
+    //console.log(allDaysList , emptyDaysList);
+    for(let day of allDaysList){
+        day.remove();
     }
 
     for(let emptyDay of emptyDaysList){
         emptyDay.remove();
     }
-    console.log(allDaysList , emptyDaysList);
+    //console.log(allDaysList , emptyDaysList);
 }
 
-function getDaysInMonth(){
-    let nrDaysInMonth = new Date(2019, 11, 0).getDate();
+function getFirstWeekdayOfMonth(setYear,setMonth){
+    let firstWeekday = new Date(setYear,setMonth, 1).getDay();
+    console.log(' first week day: '+firstWeekday);
+    return firstWeekday;//0 is sunday, 1 is monday.
+}
+
+function getDaysInMonth(setYear,setMonth){
+    let nrDaysInMonth = new Date(setYear, (setMonth+1), 0).getDate();//zero is last day of previous month
     console.log('days ' + nrDaysInMonth);
     return nrDaysInMonth;
 }
+
+function nextMonth(){
+    if(currentViewMonth === 11){ //if december
+        currentViewMonth = 0;
+        currentViewYear++;
+    }
+    else{
+        currentViewMonth++;
+    }
+    
+    console.log('current year month view: '+currentViewYear + currentViewMonth);
+    addDaysToCalendar(currentViewYear,currentViewMonth);
+}
+
+function prevMonth(){
+    if(currentViewMonth === 0){ //if january;
+        currentViewMonth = 11;
+        currentViewYear--;
+    }
+    else{
+        currentViewMonth--;
+    }
+    console.log('current year month view: '+currentViewYear + currentViewMonth);
+    addDaysToCalendar(currentViewYear,currentViewMonth);
+}
+
